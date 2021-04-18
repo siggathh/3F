a86b12e08f86048a815acdea07573bb756c8fb37
Inga Huld Ármann <ingahuldarmann@vr2t-dw314.rhi.hi.is>
2021-04-18 17:45:55 +0000

    held þetta sé reddý

--- src/sample/FlightController.java
+++ src/sample/FlightController.java
@@ -24,7 +24,7 @@ public class FlightController implements Initializable{
 
     //takes in the parameters and boolean isTo, 
     //isTo is true if searching for the flights from source to destination
-    //ifTo is false if searching for flights from destination to source
+    //isTo is false if searching for flights from destination to source
    public static ObservableList<Flight> getAvailableFlights(Parameters p, boolean isTo){
        ObservableList<Flight> availableFlights = FXCollections.observableArrayList();
 
@@ -79,6 +79,44 @@ public class FlightController implements Initializable{
         }
 
         return availableSeats;
+    }
+
+    //method that makes new booking and makes isBooked true for corrisponding seats
+    public static boolean bookFlight(String flightNumber, ArrayList<Seat> seats, Passenger passenger, int bags, int oddSized, int pillows, int blankets, boolean disability) {
+        boolean b = false;
+
+        ArrayList<Booking> bookings = new ArrayList<>();
+        Booking booking = new Booking(seats, passenger, bags, oddSized, pillows, blankets, disability);
+        bookings.add(booking);
+        passenger.setBookings(bookings);
+
+        Flight ourFlight = null;
+        for (Flight flight : DataFactory.getFlights()) {
+            if (flight.getFlightNumber().equals(flightNumber)) {
+                ourFlight = flight;
+                break;
+            }
+        }
+
+        ArrayList<Seat> flightSeats = ourFlight.getSeats();
+
+        int i = 0;
+        for (Seat seat : flightSeats) {
+            String seatNumber = seat.getSeatNumber();
+            if (i >= seats.size()) {
+                break;
+            }
+            String seatNumber2 = seats.get(i).getSeatNumber();
+            if (seatNumber.equals(seatNumber2)) {
+                seat.setBooked(true);
+                i += 1;
+            }
+        }
+        if(bookings.size() != 0){
+            b = true;
+        }
+        return b;
+
     }
 
 }    
